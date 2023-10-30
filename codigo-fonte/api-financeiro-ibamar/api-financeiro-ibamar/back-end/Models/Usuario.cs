b6a4389bf3e6d58faf3b85cd427ad2bc064831fa
Márcia Ferreira <90849504+MARCIA-BH@users.noreply.github.com>
2023-10-29 23:32:22 -0300

    add backend

---
+++ codigo-fonte/api-financeiro-ibamar/api-financeiro-ibamar/back-end/Models/Usuario.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace api_financeiro_ibamar.Models
+{
+    [Table("usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o nome!")]
+        public string Nome { get; set; }
+
+        [Display(Name = "login")]
+        [Required(ErrorMessage = "Obrigatório Informar o login!")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a senha!")]
+        [JsonIgnore]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar a senha!")]
+        public string Role { get; set; }
+
+        public string Ativo { get; set; }
+
+        
+
+        public Usuario(string Nome, string Login,String Senha, String Role, string Ativo)
+        {
+            this.Senha = Senha;
+            this.Nome = Nome;
+            this.Login = Login;
+            this.Role = Role;
+            this.Ativo = Ativo;
+        }
+
+        public Usuario()
+        {
+        }
+
+
+
+    }
+
+}
